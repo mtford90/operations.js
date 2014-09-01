667f519d85ebadf521dda699ba1ce326af3967ac
Michael Ford <mtford@gmail.com>
2014-09-01 15:12:16 +0100

    extra unit tests

--- test/cancellation.spec.js
+++ test/cancellation.spec.js
@@ -47,33 +47,68 @@ describe('Cancellation', function () {
 
     describe('composite', function () {
         var op;
-        beforeEach(function (done) {
-            var work = function (finished) {
-                var token = setInterval(function () {
-                    if (op.cancelled) {
-                        clearTimeout(token);
-                        finished();
-                    }
-                }, 20);
-            };
-            op = new Operation([new Operation('op1', work), new Operation('op2', work), new Operation('op2', work)]);
-            op.start();
-            op.cancel(done);
+
+        describe('start and then cancel', function () {
+            beforeEach(function (done) {
+                var work = function (finished) {
+                    var token = setInterval(function () {
+                        if (op.cancelled) {
+                            clearTimeout(token);
+                            finished();
+                        }
+                    }, 20);
+                };
+                op = new Operation([new Operation('op1', work), new Operation('op2', work), new Operation('op2', work)]);
+                op.start();
+                op.cancel(done);
+            });
+            it('composite op is cancelled', function () {
+                assert.ok(op.cancelled);
+                assert.notOk(op.failed);
+                assert.ok(op.completed);
+                assert.notOk(op.running);
+            });
+            it('all subops are cancelled', function () {
+                _.each(op.work, function (subOp) {
+                    assert.ok(subOp.cancelled);
+                    assert.notOk(subOp.failed);
+                    assert.ok(subOp.completed);
+                    assert.notOk(subOp.running);
+                })
+            });
         });
-        it('composite op is cancelled', function () {
-            assert.ok(op.cancelled);
-            assert.notOk(op.failed);
-            assert.ok(op.completed);
-            assert.notOk(op.running);
+
+        describe('cancel and then start', function () {
+            beforeEach(function (done) {
+                var work = function (finished) {
+                    var token = setInterval(function () {
+                        if (op.cancelled) {
+                            clearTimeout(token);
+                            finished();
+                        }
+                    }, 20);
+                };
+                op = new Operation([new Operation('op1', work), new Operation('op2', work), new Operation('op2', work)]);
+                op.cancel(done);
+                op.start();
+            });
+            it('composite op is cancelled', function () {
+                assert.ok(op.cancelled);
+                assert.notOk(op.failed);
+                assert.ok(op.completed);
+                assert.notOk(op.running);
+            });
+            it('all subops are cancelled', function () {
+                _.each(op.work, function (subOp) {
+                    assert.ok(subOp.cancelled);
+                    assert.notOk(subOp.failed);
+                    assert.ok(subOp.completed);
+                    assert.notOk(subOp.running);
+                })
+            });
         });
-        it('all subops are cancelled', function () {
-            _.each(op.work, function (subOp) {
-                assert.ok(subOp.cancelled);
-                assert.notOk(subOp.failed);
-                assert.ok(subOp.completed);
-                assert.notOk(subOp.running);
-            })
-        });
+
+
     });
 
     describe('dependencies', function () {
